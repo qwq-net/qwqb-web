2b0aa2f208a74fc269276a1596c503776464b61f
intn <me@qwqb.net>
2026-01-29 00:29:32 +0900

    cloudflare adapterのセットアップを実施

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
@@ -15,4 +17,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
